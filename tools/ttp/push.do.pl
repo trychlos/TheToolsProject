150f6da7c35cd71eecdcd971ea029db69367d463
Pierre Wieser <pierre@pwi-consulting.fr>
2025-04-05 12:42:59 +0200

    ttp.pl push honors git-check property

--- tools/ttp/push.do.pl
+++ tools/ttp/push.do.pl
@@ -1,4 +1,4 @@
-# @(#) publish code and configurations from development environment to pull target
+# @(#) publish code and configurations from a development environment to pull target
 #
 # @(-) --[no]help              print this message, and exit [${help}]
 # @(-) --[no]colored           color the output depending of the message level [${colored}]
@@ -46,7 +46,9 @@ my $defaults = {
 };
 
 my $opt_check = true;
+my $opt_check_set = false;
 my $opt_tag = true;
+my $opt_tag_set = false;
 
 # -------------------------------------------------------------------------------------------------
 # publish the development trees to the pull target
@@ -59,34 +61,39 @@ sub doPush {
 	my $command = $ep->var([ 'deployments', 'command', 'byOS', $Config{osname} ]);
 	msgVerbose( "found command='$command'" );
 	# may have several source trees: will iterate on each
-	my $sourceTrees = $ep->var([ 'deployments', 'push', 'trees' ]);
-	# if source checks are asked, then all must be OK before copying first
-	if( $opt_check ){
-		foreach my $tree ( @{$sourceTrees} ){
-			doPush_check( $tree );
+	my $trees = $ep->var([ 'deployments', 'trees' ]) || [];
+	my $count = scalar( @{$trees} );
+	if( $count ){
+		# if source checks are asked, then all must be OK before copying first
+		if( $opt_check ){
+			foreach my $tree ( @{$trees} ){
+				doPush_gitCheck( $tree );
+			}
+		} else {
+			msgWarn( "no check is made as '--check' option has been set to false" );
+		}
+		# only copy if checks have no error at all
+		if( !TTP::errs()){
+			foreach my $tree ( @{$trees} ){
+				my $res = doPush_byTree( $tree, $command );
+				$asked += $res->{asked};
+				$done += $res->{done};
+			}
+		}
+		# only tag git repositories if all copies are OK and tag has been asked and this tree allows tagging
+		if( !TTP::errs() && $done == $asked && $opt_tag ){
+			foreach my $tree ( @{$trees} ){
+				doPush_gitTag( $tree );
+			}
+		}
+		my $str = "$done/$asked copied subdir(s)";
+		if( $done == $asked && !TTP::errs()){
+			msgOut( "success ($str)" );
+		} else {
+			msgErr( "NOT OK ($str)" );
 		}
 	} else {
-		msgWarn( "no check is made as '--check' option has been set to false" );
-	}
-	# only copy if checks have no error at all
-	if( !TTP::errs()){
-		foreach my $tree ( @{$sourceTrees} ){
-			my $res = doPush_byTree( $tree, $command );
-			$asked += $res->{asked};
-			$done += $res->{done};
-		}
-	}
-	# only tag git repositories if all copies are OK and tag has been asked and this tree allows tagging
-	if( !TTP::errs() && $done == $asked && $opt_tag ){
-		foreach my $tree ( @{$sourceTrees} ){
-			doPush_tag( $tree );
-		}
-	}
-	my $str = "$done/$asked copied subdir(s)";
-	if( $done == $asked && !TTP::errs()){
-		msgOut( "success ($str)" );
-	} else {
-		msgErr( "NOT OK ($str)" );
+		msgOut( "'deployments.trees' is not set or empty: nothing to do" );
 	}
 }
 
@@ -139,50 +146,56 @@ sub doPush_byTree {
 # must publish a clean development environment from master branch
 # 'tree' is an object { source, target }
 
-sub doPush_check {
+sub doPush_gitCheck {
 	my ( $tree ) = @_;
-	msgOut( "checking source='$tree->{source}'" );
-	my @status = `git -C $tree->{source} status`;
-	my $branch = '';
-	my $changes = false;
-	my $untracked = false;
-	my $clean = false;
-	foreach my $line ( @status ){
-		chomp $line;
-		if( $line =~ /^On branch/ ){
-			$branch = $line;
-			$branch =~ s/^On branch //;
+	my $allowed = false;
+	$allowed = $tree->{'git-check'} if defined $tree->{'git-check'};
+	if( $allowed ){
+		msgOut( "checking source='$tree->{source}'" );
+		my @status = `git -C $tree->{source} status`;
+		my $branch = '';
+		my $changes = false;
+		my $untracked = false;
+		my $clean = false;
+		foreach my $line ( @status ){
+			chomp $line;
+			if( $line =~ /^On branch/ ){
+				$branch = $line;
+				$branch =~ s/^On branch //;
+			}
+			if( $line =~ /working tree clean/ ){
+				$clean = true;
+			}
+			# either changes not staged or changes to be committed
+			if( $line =~ /^Changes / ){
+				$changes  = true;
+			}
+			if( $line =~ /^Untracked files:/ ){
+				$untracked  = true;
+			}
 		}
-		if( $line =~ /working tree clean/ ){
-			$clean = true;
+		if( $branch ne 'master' ){
+			msgErr( "$tree->{source}: must publish from 'master' branch, found '$branch'" );
+		} else {
+			msgVerbose( "$tree->{source}: publishing from '$branch' branch: fine" );
 		}
-		# either changes not staged or changes to be committed
-		if( $line =~ /^Changes / ){
-			$changes  = true;
+		if( $changes ){
+			msgErr( "$tree->{source}: have found uncommitted changes, but shouldn't" );
+		} else {
+			msgVerbose( "$tree->{source}: no uncommitted change found: fine" );
 		}
-		if( $line =~ /^Untracked files:/ ){
-			$untracked  = true;
+		if( $untracked ){
+			msgErr( "$tree->{source}: have found untracked files, but shouldn't (maybe move them to uncommitted/)" );
+		} else {
+			msgVerbose( "$tree->{source}: no untracked file found: fine" );
 		}
-	}
-	if( $branch ne 'master' ){
-		msgErr( "$tree->{source}: must publish from 'master' branch, found '$branch'" );
+		if( !$clean ){
+			msgErr( "$tree->{source}: must publish from a clean working tree, but this one is not" );
+		} else {
+			msgVerbose( "$tree->{source}: found clean working tree: fine" );
+		}
 	} else {
-		msgVerbose( "$tree->{source}: publishing from '$branch' branch: fine" );
-	}
-	if( $changes ){
-		msgErr( "$tree->{source}: have found uncommitted changes, but shouldn't" );
-	} else {
-		msgVerbose( "$tree->{source}: no uncommitted change found: fine" );
-	}
-	if( $untracked ){
-		msgErr( "$tree->{source}: have found untracked files, but shouldn't (maybe move them to uncommitted/)" );
-	} else {
-		msgVerbose( "$tree->{source}: no untracked file found: fine" );
-	}
-	if( !$clean ){
-		msgErr( "$tree->{source}: must publish from a clean working tree, but this one is not" );
-	} else {
-		msgVerbose( "$tree->{source}: found clean working tree: fine" );
+		msgVerbose( "do not git-check '$tree->{source}' source tree as not allowed by the configuration" );
 	}
 }
 
@@ -190,7 +203,7 @@ sub doPush_check {
 # not all source trees are candidate to git tagging - this must be allowed in the JSON configuration
 # 'tree' is an object { source, target, git-tag }
 
-sub doPush_tag {
+sub doPush_gitTag {
 	my ( $tree ) = @_;
 	my $allowed = false;
 	$allowed = $tree->{'git-tag'} if defined $tree->{'git-tag'};
@@ -206,7 +219,7 @@ sub doPush_tag {
 			print `$command`;
 		}
 	} else {
-		msgVerbose( "do not tag '$tree->{source}' git repository as not allowed by the configuration" );
+		msgVerbose( "do not git-tag '$tree->{source}' source tree as not allowed by the configuration" );
 	}
 }
 
@@ -219,8 +232,17 @@ if( !GetOptions(
 	"colored!"			=> \$ep->{run}{colored},
 	"dummy!"			=> \$ep->{run}{dummy},
 	"verbose!"			=> \$ep->{run}{verbose},
+	"check!"			=> sub {
+		my( $name, $value ) = @_;
+		$opt_check = $value;
+		$opt_check_set = true;
+	},
 	"check!"			=> \$opt_check,
-	"tag!"				=> \$opt_tag )){
+	"tag!"				=> sub {
+		my( $name, $value ) = @_;
+		$opt_tag = $value;
+		$opt_tag_set = true;
+	})){
 
 		msgOut( "try '".$running->command()." ".$running->verb()." --help' to get full usage syntax" );
 		TTP::exit( 1 );
@@ -238,12 +260,27 @@ msgVerbose( "got check='".( $opt_check ? 'true':'false' )."'" );
 msgVerbose( "got tag='".( $opt_tag ? 'true':'false' )."'" );
 
 # check that we are pushing only on the pull reference host
-my $target_host = $ep->var([ 'deployments', 'pull', 'reference' ]);
-my $this_host = $ep->node()->name();
-if( $target_host ne $this_host ){
-	msgErr( "must push on pull reference host '$target_host', found '$this_host'" );
+my $ref_host = $ep->var([ 'deployments', 'reference' ]);
+if( $ref_host ){
+	my $this_host = $ep->node()->name();
+	if( $ref_host ne $this_host ){
+		msgErr( "must push on pull reference host '$ref_host', found '$this_host'" );
+	} else {
+		msgVerbose( "pushing on pull reference host '$ref_host': fine" );
+	}
 } else {
-	msgVerbose( "pushing on pull reference host '$target_host': fine" );
+	msgWarn( "'deployements.reference' node name expected, not found. Terminating..." );
+	TTP::exit();
+}
+
+# cannot git-tag if not previously git-checked
+if( $opt_tag && !$opt_check ){
+	if( $opt_tag_set ){
+		msgErr( "cannot git-tag if not previously git-checked" );
+	} else {
+		msgVerbose( "not git-tagging as not git-checking" );
+		$opt_tag = false;
+	}
 }
 
 if( !TTP::errs()){
