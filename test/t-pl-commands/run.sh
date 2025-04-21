56e30abe5fafa703a5d4ef6c1ef4f0b0fab31231
Pierre Wieser <p.wieser@trychlos.org>
2025-04-21 17:43:17 +0200

    Have a test suite with an almost fixed architecture

---
+++ test/t-pl-commands/run.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+# The Tools Project - Tools System and Working Paradigm for IT Production
+# Copyright (©) 1998-2023 Pierre Wieser (see AUTHORS)
+# Copyright (©) 2023-2025 PWI Consulting
+#
+# TheToolsProject is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# TheToolsProject is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with TheToolsProject; see the file COPYING. If not,
+# see <http://www.gnu.org/licenses/>.
+#
+# Check for TTP bootstrapping when site.json or node.json are not present
+#
+# (I):
+# - temp file where results counts are to be written
+# - temp file where error messages are to be written
+
+thisdir="$(cd "$(dirname "$0")"; pwd)"
+thisbase="$(basename "${thisdir}")"
+. "$(dirname "${thisdir}")/functions.sh"
+
+_toolsdir="$(dirname $(dirname "${thisdir}"))/tools"
+color_blue "[${thisbase}] checking TTP standard commands and verbs helps"
+
+# dynamically build a working environment
+rm -fr "${thisdir}/work"
+mkdir -p "${thisdir}/work/etc/ttp"
+echo "{}" > "${thisdir}/work/etc/ttp/site.json"
+mkdir -p "${thisdir}/work/etc/nodes"
+echo "{}" > "${thisdir}/work/etc/nodes/$(hostname).json"
+
+export TTP_ROOTS="${_toolsdir}:${thisdir}/work"
+export TTP_NODE=$(hostname)
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:$HOME/bin:$HOME/local/bin:${_toolsdir}/bin:${thisdir}/work/bin"
+export FPATH="${_toolsdir}/libexec/sh:${thisdir}/work/libexec/sh"
+export PERL5LIB="${_toolsdir}/libexec/perl:${thisdir}/work/libexec/perl"
+
+_fout="$(mktemp)"
+_ferr="$(mktemp)"
+
+# we are going to explore all commands, and all verbs of each command
+echo -n "  [${thisbase}] getting the list of commands... "
+(( _count_total+=1 ))
+ttp.pl list -commands 1>"${_fout}" 2>"${_ferr}"
+_rc=$?
+if [ ${_rc} -eq 0 -a -s "${_fout}" -a ! -s "${_ferr}" ]; then
+    echo "OK"
+    (( _count_ok += 1 ))
+else
+    color_red "NOTOK"
+    (( _count_notok += 1 ))
+    cat "${_fout}" >> ${_fic_errors}
+    cat "${_ferr}" >> ${_fic_errors}
+fi
+
+echo -n "  [${thisbase}] verifying the count of commands... "
+(( _count_total+=1 ))
+let -i _count_lines=$(grep -v '^\[ttp.pl list] ' "${_fout}" | wc -l)
+let -i _count_verb=$(grep 'found command' "${_fout}" | awk '{ print $3 }')
+if [ ${_count_lines} -eq ${_count_verb} ]; then
+    echo "OK"
+    (( _count_ok += 1 ))
+else
+    color_red "NOTOK"
+    (( _count_notok += 1 ))
+    cat "${_fout}" >> ${_fic_errors}
+    echo "count_lines=${_count_lines}" >> ${_fic_errors}
+    echo "count_verb=${_count_verb}" >> ${_fic_errors}
+fi
+
+# iterate on every found command
+(( _count_total+=1 ))
+for _command in $(grep -v '^\[ttp.pl list] ' "${_fout}" | sed -e 's|^\s*||' -e 's|:.*$||'); do
+
+    echo -n "  [${thisbase}] getting the list of '${_command}' available verbs... "
+    (( _count_total+=1 ))
+    ${_command} 1>"${_fout}" 2>"${_ferr}"
+    _rc=$?
+    if [ ${_rc} -eq 0 -a -s "${_fout}" -a ! -s "${_ferr}" ]; then
+        echo "OK"
+        (( _count_ok += 1 ))
+
+        # and ask the help for each verb
+        _fverb="$(mktemp)"
+        for _verb in $(grep -v "^${_command}:" "${_fout}" | sed -e 's|^s*||' -e 's|:.*$||'); do
+
+            echo -n "  [${thisbase}] asking for '${_command} ${_verb}' help... "
+            (( _count_total+=1 ))
+            ${_command} ${_verb} 1>"${_fverb}" 2>"${_ferr}"
+            _rc=$?
+            if [ ${_rc} -eq 0 -a -s "${_fverb}" -a ! -s "${_ferr}" ]; then
+                echo "OK"
+                (( _count_ok += 1 ))
+
+            else
+                color_red "NOTOK"
+                (( _count_notok += 1 ))
+                cat "${_fverb}" >> ${_fic_errors}
+                cat "${_ferr}" >> ${_fic_errors}
+            fi
+
+        done
+        rm -f "${_fverb}"
+
+    else
+        color_red "NOTOK"
+        (( _count_notok += 1 ))
+        cat "${_fout}" >> ${_fic_errors}
+        cat "${_ferr}" >> ${_fic_errors}
+    fi
+done
+
+rm -f "${_fout}"
+rm -f "${_ferr}"
+rm -fr "${thisdir}/work"
+ender
