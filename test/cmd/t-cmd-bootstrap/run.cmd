704bc5a87f1c86f6bd21039a3493af65e48a548c
Pierre Wieser <pierre@pwi-consulting.fr>
2025-04-24 18:59:26 +0200

    Add t-cmd-bootstrap test case

---
+++ test/cmd/t-cmd-bootstrap/run.cmd
@@ -0,0 +1,30 @@
+@echo off
+	rem The Tools Project - Tools System and Working Paradigm for IT Production
+	rem Copyright (C) 1998-2023 Pierre Wieser (see AUTHORS)
+	rem Copyright (C) 2023-2025 PWI Consulting
+	rem
+	rem TheToolsProject is free software; you can redistribute it and/or
+	rem modify it under the terms of the GNU General Public License as
+	rem published by the Free Software Foundation; either version 2 of the
+	rem License, or (at your option) any later version.
+	rem
+	rem TheToolsProject is distributed in the hope that it will be useful,
+	rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+	rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	rem General Public License for more details.
+	rem
+	rem You should have received a copy of the GNU General Public License
+	rem along with TheToolsProject; see the file COPYING. If not,
+	rem see <http://www.gnu.org/licenses/>.
+	rem
+	rem Check that cmd bootstrapping works
+
+	call %maindir%\functions.cmd starter "%~dp0" "checking cmd bootstrapping"
+	call :checkModules
+	call %maindir%\functions.cmd ender
+	exit /b
+
+:checkModules
+	exit /b
+
+	exit /b
