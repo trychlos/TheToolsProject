9eb8a30c8f7834dd098d7dd352f29e64c19a72f3
Pierre Wieser <pierre@pwi-consulting.fr>
2025-04-29 12:12:11 +0200

    Make bootstrap.cmd much more reliable, updating t-cmd-bootstrap accordingly

--- test/cmd/run.cmd
+++ test/cmd/run.cmd
@@ -69,7 +69,7 @@
 
 	rem List of test directories
 	rem set test_dirs=t-perl t-perl-std t-ttp-case t-ttp-load t-cmd-bootstrap t-ttp-bootstrap t-pl-commands
-	set test_dirs=t-ttp-bootstrap
+	set test_dirs=t-cmd-bootstrap
 
 	for %%D in (%test_dirs%) do (
 		if exist %maindir%\%%D\run.cmd (
