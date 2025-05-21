6c2832a66c5004a029f18921b2e324f6c27a4902
Pierre Wieser <p.wieser@trychlos.org>
2025-05-21 17:26:54 +0200

    Add tools/libexec/cmd/workload.cmd as a new TheToolsProject resource

--- tools/libexec/cmd/workload.cmd
+++ tools/libexec/cmd/workload.cmd
@@ -26,29 +26,31 @@
 	rem e.g. "C:\INLINGUA\Scripts\cmds\workload.cmd daily.morning -dummy"
 	rem Note 1: this workload.cmd adds itself the -nocolored option to every run command. You should take care that the run commands accept (if not honor) this command-line option.
 
-	set ME=[%~nx0 %1]
+	setlocal
+	set "ME=[%~nx0 %1]"
 	call :setLogFile %1
 	call :doExecute %* >>%LOGFILE% 2>&1
+	endlocal
 	exit /b
 
 :doExecute
 	call :logLine executing %~f0 %*
 	set i=0
 	for /f "tokens=*" %%C in ('services.pl list -workload %1 -commands -hidden %2 %3 %4 %5 %6 %7 %8 %9 -nocolored ^| findstr /V "[services.pl list]"') do call :doCommand %%C %2 %3 %4 %5 %6 %7 %8 %9
-	%~dp0\\workload_summary.pl -workload %1 -commands res_command -start res_start -end res_end -rc res_rc -count %i% %2 %3 %4 %5 %6 %7 %8 %9 -nocolored
+	services.pl workload-summary -workload %1 -commands res_command -start res_start -end res_end -rc res_rc -count %i% %2 %3 %4 %5 %6 %7 %8 %9 -nocolored
     exit /b
 
 :doCommand
 	rem - have a timestamped line before running each command
 	rem - prepare the end summary
-	set /A i=i+1
-	set res_command[%i%]=%*
-	set res_start[%i%]=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2% %TIME%
+	set "res_command_%i%=%*"
+	set "res_start_%i%=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2% %TIME%"
 	call :logLine %*
 	%*
 	call :logLine RC=%ERRORLEVEL%
-	set res_rc[%i%]=%ERRORLEVEL%
-	set res_end[%i%]=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2% %TIME%
+	set "res_rc_%i%=%ERRORLEVEL%"
+	set "res_end_%i%=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2% %TIME%"
+	set /A i=i+1
 	exit /b
 
 :logLine
@@ -57,7 +59,7 @@
 
 rem compute the log pathname as 'C:\myDir\dailyLogs\250521\TTP\WS12DEV1-daily.morning-20250521-050002.log'
 :setLogFile
-	for /f "tokens=2" %%a in ('ttp.pl vars -logsCommands -nocolored') do @set _logsdir=%%a
-	set _time=%TIME: =0%
-	set LOGFILE=%_logsdir%\\%COMPUTERNAME%-%1-%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%-%_time:~0,2%%_time:~3,2%%_time:~6,2%.log
+	for /f "tokens=2" %%a in ('ttp.pl vars -logsCommands -nocolored') do @set "_logsdir=%%a"
+	set "_time=%TIME: =0%"
+	set "LOGFILE=%_logsdir%\\%COMPUTERNAME%-%1-%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%-%_time:~0,2%%_time:~3,2%%_time:~6,2%.log"
 	exit /b
